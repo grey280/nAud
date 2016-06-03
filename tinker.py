bcac9955e8d339276bbf2805ff6010bc292eef6d
Grey Patterson <greypatterson@gmail.com>
2016-06-03 14:20:34 -0500

    Clean up stuff I'm not using

--- tinker.py
+++ tinker.py
@@ -18,20 +18,20 @@ class debugger():
 		# Item Types: (type of array items)
 		# Size: (size of array)
 		# (array)
-		self.localPrint("############### {}".format(message))
-		self.localPrint("Type: {}".format(type(printObj)))
-		self.localPrint("Item Types: {}".format(type(printObj[0])))
-		self.localPrint("Size: {}".format(len(printObj)))
-		self.localPrint("{}".format(printObj))
+		self.prnt("############### {}".format(message))
+		self.prnt("Type: {}".format(type(printObj)))
+		self.prnt("Item Types: {}".format(type(printObj[0])))
+		self.prnt("Size: {}".format(len(printObj)))
+		self.prnt("{}".format(printObj))
 
 	def parsePrintArray(self, printObj):
 		# Prints array for parsing, in Excel or something
 		# time real imaginary magnitude, space-delineated, with those headings at the top
-		self.localPrint("Time,Real Component,Imaginary Component,Magnitude")
+		self.prnt("Time,Real Component,Imaginary Component,Magnitude")
 		for a in range(0, len(printObj)):
-			self.localPrint("{},{},{},{}".format(a, printObj[a].real, printObj[a].imag, np.sqrt((printObj[a].real*printObj[a].real)+(printObj[a].imag*printObj[a].imag))))
+			self.prnt("{},{},{},{}".format(a, printObj[a].real, printObj[a].imag, np.sqrt((printObj[a].real*printObj[a].real)+(printObj[a].imag*printObj[a].imag))))
 
-	def localPrint(self, msg):
+	def prnt(self, msg):
 		if(self.printFile!=None):
 			print(msg, file=self.printFile)
 		else:
@@ -46,19 +46,15 @@ def scaleData(data):
 	# Scales data for output via scipy.io.wavfile: converts real components to integers
 	return np.int16(data.real/np.max(np.abs(data.real)) * 32767)
 
-def writeSample(name, data, sampleRate=44100):
-	temp = "output/{}.wav".format(name)
-	wav.write(temp, sampleRate, data)
-
-# Doing my tinkering
+# Load variables and whatnot
 parser = argparse.ArgumentParser(description='Run an FFT on a sample.')
 parser.add_argument('i', metavar='I', nargs=1, type=str, default="440hz", help='name of sample to process (reads samples/[I].wav')
 parser.add_argument('-l', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='file to write logs to')
-# parser.add_argument('-o', nargs='?', type=str, default=None, help='file to write audio output to (writes output/[O].wav')
 args = parser.parse_args()
 dbug = debugger(args.l)
 srcData = readSample(args.i[0])
 
+# Parse some stuff!
 fftD = np.fft.fft(srcData[1], int(44100/2))
 
 dbug.parsePrintArray(fftD)
