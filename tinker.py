76d4585e6b5365b17debedba8e8f91e37fed2b6c
Grey Patterson <greypatterson@gmail.com>
2016-06-03 10:36:53 -0500

    Get it all working in one go, I guess

--- tinker.py
+++ tinker.py
@@ -1,8 +1,16 @@
 import numpy				as np
 import scipy.io.wavfile		as wav
+import argparse
+import sys
 
 
 class debugger():
+	def __init__(self, printFile=None):
+		if(printFile!=None):
+			self.printFile = printFile
+		else:
+			self.printFile = None
+
 	def debugPrintArray(self, printObj, message="Debug: "):
 		# Prints a neatly-formatted readout in style:
 		# ############### (message)
@@ -10,18 +18,24 @@ class debugger():
 		# Item Types: (type of array items)
 		# Size: (size of array)
 		# (array)
-		print("############### {}".format(message))
-		print("Type: {}".format(type(printObj)))
-		print("Item Types: {}".format(type(printObj[0])))
-		print("Size: {}".format(len(printObj)))
-		print("{}".format(printObj))
+		self.localPrint("############### {}".format(message))
+		self.localPrint("Type: {}".format(type(printObj)))
+		self.localPrint("Item Types: {}".format(type(printObj[0])))
+		self.localPrint("Size: {}".format(len(printObj)))
+		self.localPrint("{}".format(printObj))
 
 	def parsePrintArray(self, printObj):
 		# Prints array for parsing, in Excel or something
 		# time real imaginary magnitude, space-delineated, with those headings at the top
-		print("time real imaginary magnitude")
+		self.localPrint("time real imaginary magnitude")
 		for a in range(0, len(printObj)):
-			print("{} {} {} {}".format(a, printObj[a].real, printObj[a].imag, np.sqrt((printObj[a].real*printObj[a].real)+(printObj[a].imag*printObj[a].imag))))
+			self.localPrint("{} {} {} {}".format(a, printObj[a].real, printObj[a].imag, np.sqrt((printObj[a].real*printObj[a].real)+(printObj[a].imag*printObj[a].imag))))
+
+	def localPrint(self, msg):
+		if(self.printFile!=None):
+			print(msg, file=self.printFile)
+		else:
+			print(msg)
 
 # Helper functions
 def readSample(name):
@@ -37,13 +51,14 @@ def writeSample(name, data, sampleRate=44100):
 	wav.write(temp, sampleRate, data)
 
 # Doing my tinkering
-dbug = debugger()
-srcData = readSample("440hz")
+parser = argparse.ArgumentParser(description='Run an FFT on a sample.')
+parser.add_argument('i', metavar='I', nargs=1, type=str, default="440hz", help='name of sample to process')
+parser.add_argument('-o', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='file to output results to')
+args = parser.parse_args()
+
+dbug = debugger(args.o)
+srcData = readSample(args.i[0])
 
 fftD = np.fft.fft(srcData[1], int(44100/2))
 
 dbug.parsePrintArray(fftD)
-
-scaled = scaleData(fftD)
-
-# writeSample("output", scaled, srcData[0])
