463b30a550caa14faa13e209128d9436110ef6d7
Grey Patterson <greypatterson@gmail.com>
2016-06-03 12:33:05 -0500

    Change parser variable name for later additions

--- tinker.py
+++ tinker.py
@@ -53,10 +53,9 @@ def writeSample(name, data, sampleRate=44100):
 # Doing my tinkering
 parser = argparse.ArgumentParser(description='Run an FFT on a sample.')
 parser.add_argument('i', metavar='I', nargs=1, type=str, default="440hz", help='name of sample to process')
-parser.add_argument('-o', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='file to output results to')
+parser.add_argument('-l', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='file to output results to')
 args = parser.parse_args()
-
-dbug = debugger(args.o)
+dbug = debugger(args.l)
 srcData = readSample(args.i[0])
 
 fftD = np.fft.fft(srcData[1], int(44100/2))
