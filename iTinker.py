45cdf8ccfb5c24716a06d877a50fffaf9140e845
Grey Patterson <greypatterson@gmail.com>
2016-06-06 12:56:37 -0500

    More tinkery stuff

--- iTinker.py
+++ iTinker.py
@@ -2,4 +2,13 @@ import plistlib
 
 data = plistlib.readPlist("./data/iTunes.plist")
 
-print(data["Tracks"])
+tracks = data["Tracks"]
+
+for track, data in tracks.items():
+	if data.get("Year", 0)==2016:
+		# print(data['Title'])
+		print(data)
+	# if tracks[track]["Year"]==2016:
+	# print(tracks[track])
+	# print(tracks[track])
+	# print("{}: {}".format(track, data['Year']))
