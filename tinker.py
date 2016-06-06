a73ea3279553186989828eb6f6bc39cb2165821a
Grey Patterson <greypatterson@gmail.com>
2016-06-06 08:53:58 -0500

    Change function names to PEP8 style

--- tinker.py
+++ tinker.py
@@ -11,38 +11,38 @@ class debugger():
 		else:
 			self.printFile = None
 
-	def debugPrintArray(self, printObj, message="Debug: "):
+	def debug_print_array(self, printObj, message="Debug: "):
 		# Prints a neatly-formatted readout in style:
 		# ############### (message)
 		# Type: (type information)
 		# Item Types: (type of array items)
 		# Size: (size of array)
 		# (array)
-		self.prnt("############### {}".format(message))
-		self.prnt("Type: {}".format(type(printObj)))
-		self.prnt("Item Types: {}".format(type(printObj[0])))
-		self.prnt("Size: {}".format(len(printObj)))
-		self.prnt("{}".format(printObj))
+		self.debug_print("############### {}".format(message))
+		self.debug_print("Type: {}".format(type(printObj)))
+		self.debug_print("Item Types: {}".format(type(printObj[0])))
+		self.debug_print("Size: {}".format(len(printObj)))
+		self.debug_print("{}".format(printObj))
 
-	def parsePrintArray(self, printObj):
+	def parse_print_array(self, printObj):
 		# Prints array for parsing, in Excel or something
 		# time real imaginary magnitude, space-delineated, with those headings at the top
-		self.prnt("Time,Real Component,Imaginary Component,Magnitude")
+		self.debug_print("Time,Real Component,Imaginary Component,Magnitude")
 		for a in range(0, len(printObj)):
-			self.prnt("{},{},{},{}".format(a, printObj[a].real, printObj[a].imag, np.sqrt((printObj[a].real*printObj[a].real)+(printObj[a].imag*printObj[a].imag))))
+			self.debug_print("{},{},{},{}".format(a, printObj[a].real, printObj[a].imag, np.sqrt((printObj[a].real*printObj[a].real)+(printObj[a].imag*printObj[a].imag))))
 
-	def prnt(self, msg):
+	def debug_print(self, msg):
 		if(self.printFile!=None):
 			print(msg, file=self.printFile)
 		else:
 			print(msg)
 
 # Helper functions
-def readSample(name):
+def read_sample(name):
 	temp = "samples/{}.wav".format(name)
 	return wav.read(temp)
 
-def scaleData(data):
+def scale_data(data):
 	# Scales data for output via scipy.io.wavfile: converts real components to integers
 	return np.int16(data.real/np.max(np.abs(data.real)) * 32767)
 
@@ -52,9 +52,9 @@ parser.add_argument('i', metavar='I', nargs=1, type=str, default="440hz", help='
 parser.add_argument('-l', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='file to write logs to')
 args = parser.parse_args()
 dbug = debugger(args.l)
-srcData = readSample(args.i[0])
+srcData = read_sample(args.i[0])
 
 # Parse some stuff!
 fftD = np.fft.fft(srcData[1], int(44100/2))
 
-dbug.parsePrintArray(fftD)
+dbug.parse_print_array(fftD)
