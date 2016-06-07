ef15d8c06dadda6faf0c0c4672e744168a0efdb8
Grey Patterson <greypatterson@gmail.com>
2016-06-07 13:30:46 -0500

    Add debugging method

--- tflow.py
+++ tflow.py
@@ -2,30 +2,40 @@ import plistlib
 import tensorflow	as tf
 
 # Variables
+debug_mode = False
 batch_size = 100
 input_data = "data/iTunes.plist"
 
-
 # Helper Functions
+def debug(string):
+	# I'm lazy, okay?
+	if debug_mode:
+		print(string)
+
 def string_to_float(original_name):
 	# Convert a string to a float: not a *strict* mapping algorithm, but probably close enough to work
+	debug("Converting {} to float.".format(original_name))
 	multiplier = 0.01
 	outvar = 0.1
 	for c in original_name:
 		outvar = outvar + (multiplier * (ord(c) - 70))
 		multiplier = multiplier * 0.1
+	debug("		Result: {}".format(abs(outvar)))
 	return abs(outvar)
 
 def scale_bit_rate(bit_rate):
 	# Scale bitrate into a float; using 1500 as the peak value bc 1411 is the highest in my library
+	debug("Scaling bit rate: {}".format(bit_rate))
 	return float(bit_rate/1500)
 
 def scale_year(year):
 	# Scale year into a float; using 2016 as it's now, abs as some people have REALLY old music
+	debug("Scaling year: {}".format(year))
 	return abs(float(year/2016))
 
 # TensorFlow Helper Funtions
 def create_placeholders(batch_size):
+	debug("Creating placeholders with batch size {}".format(batch_size))
 	input_placeholder = tf.placeholder(tf.float32, shape=(6, batch_size))
 	# make it a single placeholder that's wide, reads in each value as part of that one tensor
 	# genre_placeholder = tf.placeholder(tf.float32, shape=())
@@ -42,6 +52,7 @@ def create_placeholders(batch_size):
 def fill_feed_dict(data_set, genre_pl, year_pl, bitrate_pl, artist_pl, title_pl, rating_pl):
 	# TODO insert code to actually fill in the various _feed variables
 	#		They should each be a tensor of values
+	debug("Filling feed diciontary based on various inputs.")
 	feed_dict = {
 		genre_pl: genre_feed,
 		year_pl: year_feed,
@@ -52,9 +63,8 @@ def fill_feed_dict(data_set, genre_pl, year_pl, bitrate_pl, artist_pl, title_pl,
 	}
 	return feed_dict
 
-
-# Read in data to process
 tracks = plistlib.readPlist(input_data)["Tracks"]
+debug("Read plist")
 
 # [x] There are 55 genres in my iTunes library, use that as the scale (alphabetical order?)
 #		Just gonna use the same function as I'm using for artist/title
@@ -67,4 +77,5 @@ tracks = plistlib.readPlist(input_data)["Tracks"]
 # Ratings go from 0 to 5, inclusive
 
 input_placeholder, rating_placeholder = create_placeholders(batch_size)
-# 100 is a magic number for now, I just haven't figured out what the batch size is
+debug("Placeholders generated")
+
