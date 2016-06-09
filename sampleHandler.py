abdefaca40d4f939a6672ccdd554b4e209ae4ee7
Grey Patterson <greypatterson@gmail.com>
2016-06-09 10:35:23 -0500

    Figuring out what's necessary to process, and how

--- sampleHandler.py
+++ sampleHandler.py
@@ -2,3 +2,15 @@ import plistlib
 import gdebug
 import numpy		as np
 import subprocess
+
+d = gdebug.Debugger(debug_level = 2) # 0: off 1: errors only 2: normal 3: verbose
+input_data = "data/iTunes.plist"
+
+d.verbose("Preparing to read tracks in.")
+tracks = plistlib.readPlist(input_data)["Tracks"]
+d.debug("Tracks read.")
+
+# The kinds of files iTunes has: 'Matched AAC audio file', 'Protected MPEG-4 video file', 'MPEG audio file', 'MPEG-4 video file', 'unknown kind', 'WAV audio file', 'AAC audio file', 'Purchased AAC audio file', 'Purchased MPEG-4 video file', 'Protected AAC audio file'
+# Kinds we want to process: MPEG audio file, AAC audio file
+# Kinds we want to copy without processing:  WAV audio file
+
