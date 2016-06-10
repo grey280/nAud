dcd3764a87ec79b42d08b312ddfc54b44b297a8c
Grey Patterson <greypatterson@gmail.com>
2016-06-10 13:33:24 -0500

    Organize into functions for profiling

--- sampleReadKeras.py
+++ sampleReadKeras.py
@@ -10,6 +10,7 @@ import gdebug
 import gconvert			as conv
 
 import random			# for testing only; once the this_song = random.choice() line is gone, take this out
+from sys import getsizeof # used for profiling during testing only
 
 # Settings
 debug_mode = 3 # 0: silent, 1: errors only, 2: normal, 3: verbose
@@ -18,6 +19,56 @@ input_data = "cache/data.plist"
 
 # Tools
 d = gdebug.Debugger(debug_level = debug_mode)
+
+# Helper functions
+def parse_track(track, data):
+	global debug_counter
+	d.verbose("  Parsing track: {}".format(track))
+	debug_counter += 1
+	d.verbose("    Track {} of {}".format(debug_counter, total_count))
+
+	# Process metadata
+	title_orig = data.get("title", "unknown")
+	title = conv.string_to_int(title_orig)
+	artist_orig = data.get("artist", "unknown")
+	artist = conv.string_to_int(artist_orig)
+	year = int(data.get("year", 2016))
+	bitrate = int(data.get("bit_rate", 128))
+	genre_orig = data.get("genre", "Unknown")
+	genre = int(conv.convert_genre(genre_orig))
+	# answer_feed.append(genre)
+
+	# Process sample
+	sample_data = wav.read(track)
+	# if debug_counter == 1:
+	# 	d.debug(sample_data[1][:500])
+	d.verbose("    Samples: {}".format(len(sample_data[1])))
+	# sample_rate_feed.append(sample_data[0])
+	# sample_data = np.ndarray.flatten(sample_data[1]) # numpy is apparently the memory hog, so try without it
+	data = [int(val) for sublist in sample_data[1] for val in sublist]
+	del sample_data
+	output = [title, artist, year, bitrate]
+	d.debug("    Sample list kind: {}".format(type(data)))
+	output.extend(data)
+	return genre, output
+
+# @profile
+def parse_tracks(tracks):
+	d.debug("Building feeds.")
+	data_feed = [] # [mapped_title, mapped_artist, mapped_year, mapped_bitrate, sample_data (extended out)]
+	answer_feed = [] # mapped_genre
+	global debug_counter
+	for track, data in tracks.items():
+		genre, output = parse_track(track, data)
+		answer_feed.append(genre)
+		data_feed.append(output)
+		del output
+		del genre
+		d.debug("    Current size of data_feed: {}".format(getsizeof(data_feed)))
+		# if debug_counter >= 10:
+		# 	break
+	return data_feed, answer_feed
+
 
 # Import data
 d.debug("Start: read plist")
@@ -34,60 +85,24 @@ d.debug("End: read plist")
 #	so that it can be consistent across trainings and whatnot. I'll go write that.
 
 # Feed builders
-d.debug("Building feeds.")
-data_feed = [] # [mapped_title, mapped_artist, mapped_year, mapped_bitrate, sample_data (extended out)]
-answer_feed = [] # mapped_genre
+
+
 # sample_rate_feed = []
 
 debug_counter = 0
 total_count = len(tracks)
+data_feed, answer_feed = parse_tracks(tracks)
 
-for track, data in tracks.items():
-	d.verbose("  Parsing track: {}".format(track))
-	debug_counter += 1
-	d.verbose("    Track {} of {}".format(debug_counter, total_count))
 
-	# Process metadata
-	title_orig = data.get("title", "unknown")
-	title = conv.string_to_int(title_orig)
-	artist_orig = data.get("artist", "unknown")
-	artist = conv.string_to_int(artist_orig)
-	year = data.get("year", 2016)
-	bitrate = data.get("bit_rate", 128)
-	genre_orig = data.get("genre", "Unknown")
-	genre = conv.convert_genre(genre_orig)
-	answer_feed.append(genre)
+# np.save("tempdump1.npy", answer_feed)
+# d.debug("Saved answer feed to disk.")
 
-	# Process sample
-	sample_data = wav.read(track)
-	# if debug_counter == 1:
-	# 	d.debug(sample_data[1][:500])
-	d.verbose("    Samples: {}".format(len(sample_data[1])))
-	# sample_rate_feed.append(sample_data[0])
-	# sample_data = np.ndarray.flatten(sample_data[1]) # numpy is apparently the memory hog, so try without it
-	sample_data = [val for sublist in sample_data[1] for val in sublist]
-	output = [title, artist, year, bitrate]
-	output.extend(sample_data)
-	data_feed.append(output)
-
-	del output
-	del sample_data
-	if debug_counter >= 1:
-		break
-
-np.save("tempdump1.npy", answer_feed)
-d.debug("Saved answer feed to disk.")
-
-np.save("tempdump.npy", data_feed)
-d.debug("Saved data feed to disk.")
-
+# np.save("tempdump.npy", data_feed)
+# d.debug("Saved data feed to disk.")
 
 d.debug("Feeds constructed.")
 d.debug("{} {} {} {} {} {}".format(data_feed[0][0], data_feed[0][1], data_feed[0][2], data_feed[0][3], data_feed[0][4], data_feed[0][5]))
-print(type(data_feed[0][5]))
-d.debug(data_feed[0])
-# d.verbose("Count feed:{}".format(sample_rate_feed))
-d.debug(len(data_feed[0]))
+d.debug("Length of first data_feed item: {}".format(len(data_feed[0])))
 # model = Sequential()
 # model.add(Dense(64, input_dim=7 , init='uniform')) # 5-dim input: genre,year,bit_rate,artist,title, float-ified
 # model.add(Activation('tanh'))
