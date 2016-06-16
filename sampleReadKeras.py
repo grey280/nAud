dc3f16553413ce991aa0e3dce069704c2e430175
Grey Patterson <greypatterson@gmail.com>
2016-06-16 10:07:22 -0500

    More tests

--- sampleReadKeras.py
+++ sampleReadKeras.py
@@ -30,8 +30,8 @@ early_stopping_patience = 3 			# how many epochs without improvement it'll go be
 
 ## IO settings
 input_data = "cache/data.plist"
-weights_file_name = "start3.1.json"
-model_file_name = "start3.1.hdf5"
+weights_file_name = "start3.3.json"
+model_file_name = "start3.3.hdf5"
 vstack_split_size = 35					# controls the speed/memory usage of loading tracks. 25-50 works well.
 start_point = 0 						# seconds into the sample to read (must be within sample range)
 end_point = 10 							# seconds into the sample to stop reading (start_point<end_point<sample length)
