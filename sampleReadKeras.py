f30e4097b24afe9e7a68669ca0d4e156c30a0a67
Grey Patterson <greypatterson@gmail.com>
2016-06-29 11:18:05 -0500

    Comment

--- sampleReadKeras.py
+++ sampleReadKeras.py
@@ -27,10 +27,10 @@ early_stopping_patience = 3 			# how many epochs without improvement it'll go be
 
 ## IO settings
 input_data = "cache/data.plist"
-# weights_file_name = "nDS.json"			# name of model file to load
-# model_file_name = "nDS.hdf5"			# name of weights file to load
 test_series_name = "BGNN"				# name of the test series - files are saved as test_series_name.iteration.json/hdf5
 tests_in_series = 3 					# number of tests to run in this series
+
+## Data set settings
 vstack_split_size = 25					# controls the speed/memory usage of loading tracks. 25-50 works well.
 start_point = 60 						# seconds into the sample to read ((start_point+sample_duration)<sample length)
 sample_duration = 20					# seconds of sample to read ((start_point+sample_duration)<sample length)
