0764ad7c2a78ff5a3eeeb7bba66aee3646543cba
Grey Patterson <greypatterson@gmail.com>
2016-07-01 08:59:03 -0500

    Tweak names

--- gdebug.py
+++ gdebug.py
@@ -3,7 +3,7 @@ import sys
 
 class Debugger:
 	def __init__(self, debug_level=0):
-		self.debug_level = debug_level
+		self._debug_level = debug_level
 
 	def debug_level():
 	    doc = "Debug level: 0: none, 1: errors only, 2: normal, 3: verbose"
@@ -17,31 +17,34 @@ class Debugger:
 	debug_level = property(**debug_level())
 
 	def error(self, string):
-		if self.debug_level > 0:
+		if self._debug_level > 0:
 			print(string)
 
-	def debug(self, string):
-		if self.debug_level > 1:
+	def log(self, string):
+		if self._debug_level > 1:
 			print(string)
+	def debug(self, string):
+		self.log(string)
 
 	def verbose(self, string):
-		if self.debug_level > 2:
+		if self._debug_level > 2:
 			print(string)
+
 	def vprogress(self, message, currentval, maxval, bar_length=20):
-		if self.debug_level > 2:
+		if self._debug_level > 2:
 			pbar = self._progress_bar_(currentval, maxval, length=bar_length)
 			if not currentval == maxval:
 				sys.stdout.write("\r{}\n{} {}/{}".format(message, pbar, currentval, maxval))
 			else:
-				sys.stdout.write("\r{}\n{} complete\n".format(message, pbar))
+				sys.stdout.write("\r{}\n{} {}/{} complete    \n".format(message, pbar, currentval, maxval))
 
 	def progress(self, message, currentval, maxval, bar_length=20):
-		if self.debug_level > 1:
+		if self._debug_level > 1:
 			pbar = self._progress_bar_(currentval, maxval, length=bar_length)
 			if not currentval == maxval:
 				sys.stdout.write("\r{}: {} {}/{}".format(message, pbar, currentval, maxval))
 			else:
-				sys.stdout.write("\r{}: {} complete    \n".format(message, pbar))
+				sys.stdout.write("\r{}: {} {}/{} complete    \n".format(message, pbar, currentval, maxval))
 
 	def _progress_bar_(self, currentval, maxval, length=20):
 		output = ""
