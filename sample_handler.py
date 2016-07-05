5907af299c469ccaec155f66e42df5a1e962f974
Grey Patterson <greypatterson@gmail.com>
2016-07-05 11:11:45 -0500

    Remove tinkering

--- sample_handler.py
+++ sample_handler.py
@@ -1,8 +1,6 @@
 # sample_handler: given a file name, reads in a file and returns an array of windows
 import scipy.io.wavfile 		as wav
 import numpy					as np
-import random
-import json
 
 def read_sample(sample, window_length=1*44100):
 	read_in = wav.read(sample)
@@ -22,22 +20,3 @@ def get_sample(sample, kind, window_length=1*44100):
 	kind_arr = np.full(read.shape, kind)
 	return read, kind_arr
 
-testdict = {
-	"song1": "guitar",
-	"song2": "voice",
-	"song3": "drum",
-	"song4": "other"
-}
-# outfile = open("test.txt", "w")
-# for i in testarr:
-# 	outfile.write("{}|{}\n".format(i[0], i[1]))
-with open("data.txt", "w") as outfile:
-	json.dump(testdict, outfile, sort_keys=True, indent=4, separators=(',',': '))
-
-foo = []
-
-with open("data.txt", "r") as infile:
-	foo = json.load(infile)
-
-for i in foo:
-	print(i)
