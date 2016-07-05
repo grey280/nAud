cc12857fdcfd5b30b56f649b02c9d47fd6633b5f
Grey Patterson <greypatterson@gmail.com>
2016-07-05 11:08:13 -0500

    Playing around with JSON parsing - will use for saving database, I think

--- sample_handler.py
+++ sample_handler.py
@@ -2,6 +2,7 @@
 import scipy.io.wavfile 		as wav
 import numpy					as np
 import random
+import json
 
 def read_sample(sample, window_length=1*44100):
 	read_in = wav.read(sample)
@@ -20,3 +21,22 @@ def get_sample(sample, kind, window_length=1*44100):
 	read = read_sample("cache/{}/{}.wav".format(kind, sample), window_length)
 	kind_arr = np.full(read.shape, kind)
 	return read, kind_arr
+
+t1 = ["song1", "guitar"]
+t2 = ["song2", "voice"]
+t3 = ["song3", "drum"]
+t4 = ["song4", "other"]
+testarr = [t1, t2, t3, t4]
+# outfile = open("test.txt", "w")
+# for i in testarr:
+# 	outfile.write("{}|{}\n".format(i[0], i[1]))
+with open("data.txt", "w") as outfile:
+	json.dump(testarr, outfile)
+
+foo = []
+
+with open("data.txt", "r") as infile:
+	foo = json.load(infile)
+
+for i in foo:
+	print(i)
