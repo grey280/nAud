498231d33dae67e7c682ecfae5171cc867459367
Grey Patterson <greypatterson@gmail.com>
2016-07-06 10:05:41 -0500

    More debug code!

--- evaluate.py
+++ evaluate.py
@@ -16,12 +16,14 @@ log_level = 2 					# 0: silent, 1: errors only, 2: normal, 3: verbose
 ## IO settings
 test_series_name = "INS1"		# name of the test series - files are saved as test_series_name.iteration.json/hdf5
 window_size = 1*44100 			# size of windows to feed
+file_to_read = "cache/test/LizNelson_Rainfall_MIX.wav" # path to wav file to read
 
 ## Operational settings
 do_load_model = False
 do_load_weights = False
-load_from_previous_trial = False
-trial_iteration = 0 			# Which iteration of the trial series are you on? Used to load/save. Starts at 0.
+load_from_previous_trial = True
+trial_iteration = 1 			# Which iteration of the trial series are you on? Used to load/save. Starts at 0.
+which_iteration = 0 			# Which iteration to load
 
 # Tools
 d = gdebug.Debugger(debug_level = log_level)
@@ -44,11 +46,24 @@ def load_weights(iteration=0, path=test_series_name):
 	model.load_weights(load_path)
 
 # Multi-iteration crossing
-model = load_model(iteration=i)
+model = load_model(iteration=which_iteration)
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-load_weights(iteration=i)
+load_weights(iteration=which_iteration)
 d.debug("Neural network loaded.")
+
+test_data = data_handler.get_samples_from_file(file_to_read)
+results = []
+for sample in test_data:
+	score = model.predict(sample, batch_size=1)
+	results.append(score)
+
+print("Analyzed {} samples, yielding {} results.".format(len(test_data),len(results)))
+
+for result in results:
+	pass
+	# print(result)
+
 # Evaluate against test data
 # test_data, test_answers, test_info_feed = data_set.next_test_batch(evaluation_data_point_count)
 # del test_info_feed
