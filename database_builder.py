8a8ef5eac21d48e56adc6fdd397eeef212e7a13e
Grey Patterson <greypatterson@gmail.com>
2016-07-11 09:04:49 -0500

    Working on more database

--- database_builder.py
+++ database_builder.py
@@ -5,6 +5,15 @@ working_file = "data/database.json"
 # 	example provided
 songs_to_add={
 	# "LizNelson_Rainfall_STEM_04": "guitar"
+	"AClassicEducation_NightOwl_STEM_02": "drum",
+	"AClassicEducation_NightOwl_STEM_05": "guitar",
+	"AClassicEducation_NightOwl_STEM_08": "vocal",
+	"AClassicEducation_NightOwl_MIX": "other",
+	"AimeeNorwich_Child_STEM_01": "drum",
+	"AimeeNorwich_Child_STEM_03": "guitar",
+	"AimeeNorwich_Child_MIX": "other",
+	"AimeeNorwich_Child_STEM_07": "guitar",
+	"AimeeNorwich_Child_STEM_04": "vocal"
 }
 
 database = {}
