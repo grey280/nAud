49c535142d955ff155f09f27a86ba68b240a6329
Grey Patterson <greypatterson@gmail.com>
2016-07-11 09:30:11 -0500

    Add more songs to database, and let's train

--- database_builder.py
+++ database_builder.py
@@ -13,7 +13,30 @@ songs_to_add={
 	"AimeeNorwich_Child_STEM_03": "guitar",
 	"AimeeNorwich_Child_MIX": "other",
 	"AimeeNorwich_Child_STEM_07": "guitar",
-	"AimeeNorwich_Child_STEM_04": "vocal"
+	"AimeeNorwich_Child_STEM_04": "vocal",
+	"AlexanderRoss_GoodbyeBolero_MIX": "other",
+	"AlexanderRoss_GoodbyeBolero_STEM_01": "guitar",
+	"AlexanderRoss_GoodbyeBolero_STEM_02": "guitar",
+	"AlexanderRoss_GoodbyeBolero_STEM_03": "guitar",
+	"AlexanderRoss_GoodbyeBolero_STEM_05": "drum",
+	"AlexanderRoss_GoodbyeBolero_STEM_06": "vocal",
+	"AlexanderRoss_VelvetCurtain_MIX": "other",
+	"AlexanderRoss_VelvetCurtain_STEM_01": "guitar",
+	"AlexanderRoss_VelvetCurtain_STEM_02": "guitar",
+	"AlexanderRoss_VelvetCurtain_STEM_03": "guitar",
+	"AlexanderRoss_VelvetCurtain_STEM_05": "drum",
+	"AlexanderRoss_VelvetCurtain_STEM_06": "vocal",
+	"AlexanderRoss_VelvetCurtain_STEM_10": "guitar",
+	"AmarLal_Rest_STEM_01": "guitar",
+	"AmarLal_SpringDay1_STEM_01": "guitar",
+	"Auctioneer_OurFutureFaces_MIX": "other",
+	"Auctioneer_OurFutureFaces_STEM_04": "drum",
+	"Auctioneer_OurFutureFaces_STEM_05": "guitar",
+	"Auctioneer_OurFutureFaces_STEM_08": "vocal",
+	"Auctioneer_OurFutureFaces_STEM_09": "drum",
+	"AvaLuna_Waterduct_MIX": "other",
+	"AvaLuna_Waterduct_STEM_08": "vocal",
+	"AvaLuna_Waterduct_STEM_04": "drum"
 }
 
 database = {}
